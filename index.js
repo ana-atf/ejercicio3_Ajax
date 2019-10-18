d60d1cdb7464cfbce6b8f406a518bdc1ade4d26b
Ana <anatorres.atf@gmail.com>
2019-10-18 12:13:44 +0200

    funciones para obtener datos

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function consultarDatos(city) {
     for (i = 0; i < city.consolidated_weather.length; i++) {
         humedad.push(city.consolidated_weather[i].humidity)
     }
-    document.querySelectorAll("input")[3].value = humedad[0]
+    document.querySelectorAll("input")[3].value = humedad[0] + "%"
 
 }
 
@@ -94,6 +94,85 @@ datosMarseille.addEventListener("click", function () {
     httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/610264/", imprime)
 }
 )
+
+//Germany
+
+let datosBerlin = document.querySelector("#berlin")
+datosBerlin.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/638242/", imprime)
+}
+)
+let datosFrankfurt = document.querySelector("#frankfurt")
+datosFrankfurt.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/650272/", imprime)
+}
+)
+let datosMunich = document.querySelector("#munich")
+datosMunich.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/676757/", imprime)
+}
+)
+let datosDusseldorf = document.querySelector("#dusseldorf")
+datosDusseldorf.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/646099/", imprime)
+}
+)
+
+//Greece
+
+let datosAthens = document.querySelector("#athens")
+datosAthens.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/946738/", imprime)
+}
+)
+let datosSantorini = document.querySelector("#santorini")
+datosSantorini.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/56558361/", imprime)
+}
+)
+
+//Italy
+
+let datosRome = document.querySelector("#rome")
+datosRome.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/721943/", imprime)
+}
+)
+let datosVenice = document.querySelector("#venice")
+datosVenice.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/725746/", imprime)
+}
+)
+let datosMilan = document.querySelector("#milan")
+datosMilan.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/718345/", imprime)
+}
+)
+
+//Spain
+
+let datosBarcelona = document.querySelector("#barcelona")
+datosBarcelona.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/753692//", imprime)
+}
+)
+let datosMadrid = document.querySelector("#madrid")
+datosMadrid.addEventListener("click", function () {
+
+    httpGet("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/766273/", imprime)
+}
+)
+
 
 
 
